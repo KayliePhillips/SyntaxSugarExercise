2fb1e1488a8c456a5135a07186ae8e54f3f3cdb6
Kaylie Phillips <kaylie.travel@gmail.com>
2022-09-13 18:48:16 -0500

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿//Exercise 1
+//Given the following code, change the code to implement:
+//inferred typing, string interpolation, and the trnary operator.
+
+//int answer = 4;
+//string response;
+//if (answer <9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//    response = answer + " is greater than or equal to nine";
+//}
+//Console.WriteLine(response);
+
+var answer = 15;
+var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+Console.WriteLine(response);
